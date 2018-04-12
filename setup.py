360326f06e74068c6112497d5d2e87741adfa094
bottlenome <bottlenome@gmail.com>
2018-04-12 14:17:59 +0000

    add find_package to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='mlem',
     version='0.0.1',
+    packages=find_packages("mlem")
     install_requires=['numpy', 'Pillow'],
     tests_require=['pytest']
 )
