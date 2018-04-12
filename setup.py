a6c8122a47540d8e0a51f6720365b1bb54ea880a
bottlenome <bottlenome@gmail.com>
2018-04-12 13:46:10 +0000

    Modify dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='mlem',
     version='0.0.1',
-    install_requires=['numpy', 'PIL'],
+    install_requires=['numpy', 'Pillow'],
     tests_require=['pytest']
 )
