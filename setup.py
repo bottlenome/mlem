c623b5868daa64df09cdbfb15e00d1b48b0c3026
bottlenome <bottlenome@gmail.com>
2018-04-15 13:49:04 +0000

    Modify package construction

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='mlem',
     version='0.0.1',
-    packages=find_packages("mlem"),
+    packages=["mlem"],
     install_requires=['numpy', 'Pillow'],
     tests_require=['pytest'],
-    package_dir={'': 'mlem'}
+    license='MIT'
 )
